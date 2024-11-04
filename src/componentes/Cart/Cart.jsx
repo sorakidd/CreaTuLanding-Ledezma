16632f5ea9a085beee1257ca72128d026ac89bdc
sorakidd <lucaledezma@hotmail.com>
2024-11-04 18:16:08 -0300

    cuarto commit

---
+++ src/componentes/Cart/Cart.jsx
@@ -0,0 +1,31 @@
+import './Cart.css'
+import { useContext } from "react" 
+import CartContext from '../context/CartContext'
+
+import { Link , NavLink } from 'react-router-dom' 
+import CartItem from '../CartItem/CartItem'
+
+
+const Cart = () => {
+  const [cart, clearCart, totalQuantity, total] = useContext(CartContext)
+
+  if (totalQuantity === 0) {
+    return (
+      <div>
+        <h2>No hay items en el carrito</h2>
+        <Link to='/' className='Option'>Productos</Link>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {cart.map(p => <CartItem key={p.id} {...p} />)}
+      <h3>Total: ${total}</h3>
+      <button onClick={() => clearCart()} className="Button">Limpiar carrito</button>
+      <Link to='/checkout' className='Option'>Checkout</Link>
+    </div>
+  )
+}
+
+export default Cart;
